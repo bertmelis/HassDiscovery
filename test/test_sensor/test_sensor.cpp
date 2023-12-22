4ab6574e8bd1a3b6eb8b0ade52e89532161c8502
Bert Melis <bertmelis@users.noreply.github.com>
2023-12-22 14:16:12 +0100

    add discovery target

--- test/test_sensor/test_sensor.cpp
+++ test/test_sensor/test_sensor.cpp
@@ -6,6 +6,34 @@ For a copy, see <https://opensource.org/licenses/MIT> or
 the LICENSE file.
 */
 
+/*
+
+homeassistant/binary_sensor/device01/sensor01/config
+{
+    "~": "homeassistant/device01",
+    "name": "Sensor 01",
+    "uniq_id": "device01_sensor01",
+    "opt": false,
+    "avty": [
+        {
+            "t": "~/$system/status",
+            "pl_avail": "online",
+            "pl_not_avail": "offline"
+        }
+    ],
+    "avty_mode": "latest",
+    "dev": {
+        "ids": [
+            "device01"
+        ],
+        "name": "Device 01"
+    },
+    "stat_t": "~/sensor01",
+    "pl_on": 1,
+    "pl_off": 0
+}
+*/
+
 #include <unity.h>
 
 #include <HassDiscovery.h>
@@ -14,8 +42,8 @@ void setUp() {}
 void tearDown() {}
 
 void test_binarysensor() {
-  HassDiscovery::BinarySensor device("myuniqueid");
-  const char* expectedTopic = "homeassistant/binary_sensor/myuniqueid/config";
+  HassDiscovery::BinarySensor device("device01");
+  const char* expectedTopic = "homeassistant/binary_sensor/device01/sensor01/config";
   const char* expectedPayload = R"foo({"~":"basetopic/myuniqueid","name":"sensorname","uniq_id":"myuniqueid","opt":false,"avty":[{"t":"~/$system/status","pl_avail":"online","pl_not_avail":"offline"}],"avty_mode":"latest","dev":{"ids":["myuniqueid"]},"stat_t":"~/sensorname","pl_on":1,"pl_off":0})foo";
 
   TEST_ASSERT_TRUE(device.create("sensorname"));
